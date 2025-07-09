04592e5fdafa343b35a6fb0e053d381f6480618c
Gabriel Abade <161393183+gabrielvitorabade@users.noreply.github.com>
2025-07-08 21:46:43 -0400

    Refatora o formulário de tarefas para gerenciar estado e adicionar funcionalidade de envio

--- todo-list/src/components/Todoform.jsx
+++ todo-list/src/components/Todoform.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import { useState } from "react";
 
 export default function Todoform() {
+    const [tdname, setTdname] = useState('')
+    const [tdcategory, setTdcategory] = useState('')
+
+    const handleAddTodo = (e) => {
+        e.preventDefault();
+     console.log(tdname, tdcategory);
+        if(!tdname || !tdcategory){
+            return
+        };
+        setTdcategory('');
+        setTdname('');
+    }
   return (
     <div>
         <h2>Criar Nova Tarefa</h2>
-        <input type="text" placeholder='Títúlo da tarefa...'/>
-        <select>
-            <option value="">Selecione a categoria</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Pessoal">Pessoal</option>
-            <option value="Estudo">Estudo</option>
-        </select>
-        <div id='add-button_box'>
-            <button id='add-button'>Adicionar Tarefa</button>
-        </div>
+        <form onSubmit={handleAddTodo}>
+            <input value={tdname} type="text" placeholder='Títúlo da tarefa...' onChange={(e)=>setTdname(e.target.value)}/>
+            <select value={tdcategory} onChange={(e)=>setTdcategory(e.target.value)}>
+                <option value="">Selecione a categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudo">Estudo</option>
+            </select>
+            <div id='add-button_box'>
+                <button id='add-button' type="submit">Adicionar Tarefa</button>
+            </div>
+        </form>
     </div>
   )
 }
